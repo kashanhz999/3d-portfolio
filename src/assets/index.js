872e6b54b7cf7ac6bc82cef2a7a76ca2b5065946
Kashan Haider <19cs162@jssaten.ac.in>
2023-05-11 21:09:21 +0530

    added Next.js icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import next from "./tech/next.png"
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -68,5 +69,6 @@ export {
   jss,
   talk,
   promptpeller,
-  sellista
+  sellista,
+  next
 };
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import {BallCanvas} from './canvas'
 import { technologies } from '../constants'
 import { SectionWrapper } from '../hoc'
+import { styles } from '../styles'
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10 '>
 
       {technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
+        <div className='w-28 h-28 text-center' key={technology.name}>
           <BallCanvas icon={technology.icon}/>
+          <h6 className="font-poppins">{technology.name}</h6>
         </div>
       ))}
     </div>
