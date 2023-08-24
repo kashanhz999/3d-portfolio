0796455e3c7bbe062ef404ca6b1ff8661bea3cfa
Kashan Haider <19cs162@jssaten.ac.in>
2023-08-24 21:48:03 +0530

    content update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,15 @@ const Hero = () => {
       top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5 ">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-5 h-5 rounded-full bg-color_hero" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Kashan</span>
+            Hi, I'm <span className="text-col">Kashan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2  text-white-100  `}>
-            I leverage my development skills to create user-friendly interfaces
+            I use my development skills to create user-friendly interfaces
             and web applications.
           </p>
         </div>
